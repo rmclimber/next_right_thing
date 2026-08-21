fbf3acb2ab316911058c020405a4a5f401a2ccf0
RM <rmorris.code@gmail.com>
2026-08-21 06:17:05 -0700

    longer timeout to create some headroom

--- backend/nrt_backend/shared/database.py
+++ backend/nrt_backend/shared/database.py
@@ -63,5 +63,5 @@ def connect():
         dbname=config["dbname"],
         user=credentials["user"],
         password=credentials["password"],
-        connect_timeout=15,
+        connect_timeout=30,
     )
